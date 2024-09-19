9216ecf1ec7befac3754d71c76016d726733ce17
Alexandr <mango.2004@yandex.ru>
2024-09-19 19:49:19 +0300

    Добавил шапку, подлключил стили, сделал главный контейнер

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1>MyApp</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
