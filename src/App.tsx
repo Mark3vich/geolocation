166d11102208e5981ab118b9b55b5f607221224f
Alexandr <mango.2004@yandex.ru>
2024-09-20 08:35:52 +0300

    Убран лишний route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
-import Table from './components/Table/Table';
 
 function App() {
   return (
